9332c3487ef3a7eaff25557cbdce31e89b241587
Andrei Petrov <thepetrovich@yktaero.space>
2022-10-27 13:25:41 +0800

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -34,7 +34,7 @@ do
 
   for i in 1 2 3 4; 
   do
-    response=$(timeout 5 curl --write-out '%{http_code}' --silent --output /dev/null $url)
+    response=$(timeout 5 curl --write-out '%{http_code}' --silent --output --connection-timeout 5 /dev/null $url)
     if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
       result="success"
     else
